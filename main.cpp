9411faf6e8f38b987f2d26d637c81198bf3f3220
Yaser Ahmed N <41563530+yaserahmedn@users.noreply.github.com>
2020-11-08 18:59:45 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[]){
+
+	string outText, imPath = argv[1];
+
+	Mat im = cv::imread(imPath, IMREAD_COLOR);
+
+	tesseract::TessBaseAPI *ocr = new tesseract::TessBaseAPI();
+
+	ocr->Init(NULL, "eng", tesseract::OEM_LSTM_ONLY);
+
+	ocr->SetPageSegMode(tesseract::PSM_AUTO);
+
+	ocr->SetImage(im.data, im.cols, im.rows, 3, im.step);
+
+	outText = string(ocr->GetUTF8Text());
+
+	cout << outText;
+
+	ofstream outfile;
+
+	outfile.open("Output_file.txt", std::ios_base::trunc); // app in place of trunc to append instead of overwrite
+	
+	outfile << outText; 
+
+	ocr->End();
+
+	return 0;
+}
